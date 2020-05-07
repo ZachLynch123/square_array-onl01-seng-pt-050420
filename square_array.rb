6774f8f8dbdd73f874d0537b028ce05dffd972d1
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-07 17:38:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   new_array = []
   count = 0
-  array.each|number| new_array.push(array[count] ^ 2) count += 1}
+  array.each do |number|
+    new_array.push(array[count] ^ 2)
+    count += 1
+  end
   return new_array
 end
